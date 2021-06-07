1c0eed3c8114023edef157980eb16dc7d2470fef
XiaojianChen8 <34709456+XiaojianChen8@users.noreply.github.com>
2021-06-07 20:04:20 +0800

    Update LMD.py

--- LMD.py
+++ LMD.py
@@ -1,2 +1,3 @@
-# Relevant research has been accepted by Computers, Environment and Urban System in 2021/05/22
-# Code will be open around 2021/06/01
+# Relevant research has been accepted by Computers, Environment and Urban System in 2021/05/22.
+# The work is online in 2021/06/01
+# We are organizing and testing the code, which will be open around 2021/06/15.
